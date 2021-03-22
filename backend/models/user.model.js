d0439893f95b9e936daf9c43267d4871ee9c90e3
rasikadeshpande24 <71489609+rasikadeshpande24@users.noreply.github.com>
2021-03-22 22:03:47 +0530

    completed

---
+++ backend/models/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+// const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const userSchema = new Schema(
+//   {
+//     username: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       trim: true,
+//       minlenght: 2,
+//     },
+//   },
+//   { timestamps: true }
+// );
+// const User = mongoose.model("User", userSchema);
+
+// module.export = User;
